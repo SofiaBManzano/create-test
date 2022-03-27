ad5d590f834603f9dbe9d9dcfb88b07d6d6bc38f
SofiaBManzano <sofiabmanzano@gmail.com>
2022-03-27 19:48:48 +0200

    readme

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,16 @@
-'use strict';
-
-console.log('>> Ready :)');
+"use strict";
+const input = document.querySelector(".js-input");
+const btn = document.querySelector(".js-btn");
+const title = document.querySelector(".js-title");
+const reset = document.querySelector(".js-reset");
+const handleBtnClick = (ev) => {
+  ev.preventDefault();
+  const inputValue = input.value;
+  title.innerHTML = inputValue;
+};
+const handleReset = (ev) => {
+  ev.preventDefault();
+  title.innerHTML = "LayoutDeEjemplo";
+};
+btn.addEventListener("click", handleBtnClick);
+reset.addEventListener("click", handleReset);
